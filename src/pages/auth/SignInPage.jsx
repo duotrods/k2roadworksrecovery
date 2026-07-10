b8af86cc562acf53dff97bffbd5457a81bd0fd28
Rod <duotrodolinor@gmail.com>
2026-07-10 20:40:06 +0800

    change the logo to k2 in the sign in page and sign up page

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,6 +1,6 @@
 import SignInForm from "../../components/auth/SignInForm";
 import heroImage from "../../assets/signbg.jpg";
-import logomarkdark from "../../assets/Logo White.svg";
+import logomarkdark from "../../assets/k2logo.svg";
 
 const SignInPage = () => {
   return (
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import SignUpForm from '../../components/auth/SignUpForm';
 import heroImage from "../../assets/signbg.jpg";
-import logomarkdark from "../../assets/Logo White.svg";
+import logomarkdark from "../../assets/k2logo.svg";
 
 const SignUpPage = () => {
   return (
